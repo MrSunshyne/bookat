5709d38cc2b086801721122b675409367eeff6a4
Nader Toukabri <nader.toukabri@gmail.com>
2017-07-07 01:54:21 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <md-toolbar>
+      <h1 class="md-title">Bookat</h1>
+    </md-toolbar>
+    <main>
+      <img src="./assets/logo.png" alt="Bookat">
+      <router-view></router-view>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+}
+
+main {
+  padding: 16px;
+}
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div>
+      <md-button>Default</md-button>
+      <md-button class="md-primary">Primary</md-button>
+      <md-button class="md-accent">Accent</md-button>
+      <md-button class="md-warn">Warn</md-button>
+      <md-button class="md-primary" disabled>Disabled</md-button>
+      <md-button class="md-dense">Dense</md-button>
+    </div>
+    <div>
+      <md-button class="md-raised">Default</md-button>
+      <md-button class="md-raised md-primary">Primary</md-button>
+      <md-button class="md-raised md-accent">Accent</md-button>
+      <md-button class="md-raised md-warn">Warn</md-button>
+      <md-button class="md-raised md-primary" disabled>Disabled</md-button>
+      <md-button class="md-raised md-dense">Dense</md-button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data() {
+    return {
+      msg: 'Welcome to Your Bookat',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
